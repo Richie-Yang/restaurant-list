f57beb1859548efea76b51b5ee63cc6c4d0dfdb0
Richie-Yang <richieyang8813@gmail.com>
2021-10-27 22:38:38 +0800

    refactor: polish newAndEdit.js and delete.js

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -2,11 +2,15 @@
 const deleteButtons = document.querySelectorAll('.delete-button')
 
 
+// loop all queried delete buttons DOM object
 deleteButtons.forEach(deleteButton => {
+  // attach event listeners for each of them 
   deleteButton.addEventListener('click', function onDeleteButtonClicked(event) {
     const id = deleteButton.dataset.id
 
+    // trigger SweetAlert package for dialogue
     Swal.fire({
+      // SweetAlert dialogue configuration
       title: '確定要刪除嗎?',
       text: "一但執行，刪除的檔案是無法恢復的唷!",
       icon: 'warning',
@@ -16,6 +20,7 @@ deleteButtons.forEach(deleteButton => {
       confirmButtonText: '確定!',
       cancelButtonText: '取消!'
     }).then(result => {
+      // if confirmed, then return something
       if (result.isConfirmed) {
         return Swal.fire(
           '檔案已經刪除!',
@@ -24,6 +29,7 @@ deleteButtons.forEach(deleteButton => {
         )
       }
     }).then(result => {
+      // if catch returned object, then delete
       if (result) {
         axios.delete(`/restaurants/${id}`)
           .then(() => window.location.href = '/')
--- public/javascripts/newAndEdit.js
+++ public/javascripts/newAndEdit.js
@@ -7,39 +7,57 @@ const saveButton = document.querySelector('#save-button')
 let progressValue = 0
 
 
-previewLoadButton.addEventListener('click', function onPreviewLoadButtonClicked(event) {
-  const newImageSource = document.querySelector('#image').value
-  let timer
-
-  if (progressValue >= 100) {
-      previewProgress.innerHTML = `
+////// Function Section Starts Here //////
+function renderPreviewProgress(element) {
+  element.innerHTML = `
       <div id="preview-progress-bar" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 0%" aria-valuenow="100" aria-valuemin="0"
       aria-valuemax="100"></div>
     `
+}
+
+function previewProgressBarFinished(element) {
+  element.classList.remove('progress-bar-striped', 'progress-bar-animated')
+  element.classList.add('bg-success')
+}
+
+function formCheck(form, event) {
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
   }
+
+  form.classList.add('was-validated')
+}
+////// Function Section Ends Here //////
+
+
+////// Event Listener Section Starts Here //////
+previewLoadButton.addEventListener('click', function onPreviewLoadButtonClicked(event) {
+  // interval timer pre-declaration
+  let timer
+
+  if (progressValue >= 100) renderPreviewProgress(previewProgress)
   const previewProgressBar = document.querySelector('#preview-progress-bar')
+  const newImageSource = document.querySelector('#image').value
 
   new Promise(resolve => {
+    // constantly checking if progress value hits over 100%
     timer = setInterval(() => {
       previewProgressBar.style.width = `${progressValue += 1}%`
       if (progressValue >= 100) resolve()
     }, 1)
   }).then(() => {
+    // clean interval timer
     clearInterval(timer)
+    // change to new preview image after one sec delay
     setTimeout(() => {
       previewImage.src = newImageSource
-      previewProgressBar.classList.remove('progress-bar-striped', 'progress-bar-animated')
-      previewProgressBar.classList.add('bg-success')
+      previewProgressBarFinished(previewProgressBar)
     }, 1000) 
   })
-
 })
 
 newEditForm.addEventListener('submit', function onNewEditFormClicked(event) {
-  if (!newEditForm.checkValidity()) {
-    event.preventDefault()
-    event.stopPropagation()
-  }
-
-  newEditForm.classList.add('was-validated')
+  formCheck(newEditForm, event)
 })
+////// Event Listener Section Ends Here //////
